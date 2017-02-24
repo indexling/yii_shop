1a955f758d7283e8efce564c626bc17daa1ab989
luoluo <502636491@qq.com>
2017-02-24 16:50:52 +0800

    //新增一控制器

---
+++ advanced/backend/controllers/CateController.php
@@ -0,0 +1,27 @@
+<?php
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class CateController extends Controller
+{
+	/**
+	 * 商品分类接口
+	 * @return 	 json
+	 */
+	public function action()
+	{
+
+	} 
+
+
+
+
+
+
+
+
+
+
+}
